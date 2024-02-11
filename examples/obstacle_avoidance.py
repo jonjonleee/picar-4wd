a9242ee05ce445af49adc1a8f3804f44b99d9650
jonjonleeee <77803918+jonjonleeee@users.noreply.github.com>
2024-02-11 00:41:17 -0600

    obstacle avoidance

--- examples/obstacle_avoidance.py
+++ examples/obstacle_avoidance.py
@@ -1,8 +1,11 @@
 import picar_4wd as fc
+import random
+import time
 
 speed = 30
 
 def main():
+    prev = 0
     while True:
         scan_list = fc.scan_step(35)
         if not scan_list:
@@ -11,9 +14,17 @@ def main():
         tmp = scan_list[3:7]
         print(tmp)
         if tmp != [2,2,2,2]:
-            fc.turn_right(speed)
+            if not prev:
+                fc.backward(speed)
+                time.sleep(0.2)
+                prev = random.randint(1,2)
+            if prev == 1:
+                fc.turn_left(speed)
+            else:
+                fc.turn_right(speed)
         else:
             fc.forward(speed)
+            prev = 0
 
 if __name__ == "__main__":
     try: 
